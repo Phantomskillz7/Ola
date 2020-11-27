13e2fc8bb72e77c4e486d4e2e7ac41b5bff3f058
Phantomskillz7 <44521773+Phantomskillz7@users.noreply.github.com>
2020-11-27 08:39:25 +0500

    ayy

---
+++ Lab9Task3testingandfixing.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int stringCounter(string sentence,string my_word)
+{   
+    int position=0,wordlength,numrepeat;
+    wordlength=my_word.length();
+
+    while(position != (sentence.length()-wordlength+1))
+    {
+      if (sentence.substr(position,wordlength)== my_word)
+      {
+        numrepeat=numrepeat+1;
+      }
+      position=position+1;
+    }
+
+return numrepeat;
+}
+
+int main()
+{
+
+string str, strword;
+int numberrepeat;
+
+cout<<"Enter string: ";
+getline(cin,str);
+cout<<"Enter word: ";
+getline(cin,strword);
+
+numberrepeat=stringCounter(str,strword);
+
+cout<<strword<< " occurs "<<numberrepeat<< " time(s) in the given string"<<endl;
+}
